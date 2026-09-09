71abf2288155ac9e8ea51ebce953f4c8acf16050
xdsun777 <charmgo@qq.com>
2026-09-10 04:34:23 +0800

    新增别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -29,4 +30,10 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets':path.resolve(__dirname,'./src/assets')
+    }
+  }
 }));
